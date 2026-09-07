92c8248d8a9cc0afcb558052dda050515f5cc369
Brianna Dongmo Nguimdo <bdnguimdo@gmail.com>
2026-09-07 00:21:24 +0000

    feat: add swim goals

---
+++ launch_console.py
@@ -0,0 +1,18 @@
+name = input("What's yoour name?")
+print("Welcome to "+ name + "'s Launch Console!")
+
+running = True
+while running:
+    print("1) About Me")
+    print("2) My Goals")
+    print("3) Exit")
+    choice = input("Pick 1-3: ")
+    if choice =="1":
+        print("...your about-me text...")
+    elif choice == "2":
+        print("My goal is to make Nationals this season for Swimming.")
+    elif choice =='3':
+        print("Goodbye!")
+        running = False
+    else:
+        print("Please pick 1, 2, or 3.")
